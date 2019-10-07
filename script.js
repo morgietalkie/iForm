b0f79aabbbacf4b623c0bd98dfbc87b07aca52d8
=Frederik <=fred84e0@stud.kea.dk>
2019-10-07 11:15:23 +0200

    header/burgermenu

--- script.js
+++ script.js
@@ -2,4 +2,17 @@
 
 document.addEventListener("DOMContentLoaded", start);
 
+const burgerMenu = document.querySelector("#burger_menu");
+document.querySelector("#burger_button").addEventListener("click", openSidebar);
+
+function openSidebar() {
+  console.log("Burgermenu toggled");
+
+  if (!(burgerMenu.style.display == "block")) {
+    burgerMenu.style.display = "block";
+  } else {
+    burgerMenu.style.display = "none";
+  }
+}
+
 function start() {}
