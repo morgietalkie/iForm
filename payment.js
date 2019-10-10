ac40b5388f59f089f96dfd3eeb06b4a20cefc7f1
Oliver <oliv5773@stud.kea.dk>
2019-10-10 12:42:56 +0200

    javascript sat op, så korrekt localstorage-info vises på de forskellige sider

--- payment.js
+++ payment.js
@@ -15,7 +15,103 @@ function openSidebar() {
   }
 }
 
-function start() {}
+function start() {
+  let localOffer = localStorage.getItem("offer");
+  if (localOffer == "offer1") {
+    document.querySelector(".chosen-offer").textContent = "4 numre af iFORM";
+  }
+  if (localOffer == "offer2") {
+    document.querySelector(".chosen-offer").textContent = "6 numre af iFORM";
+  }
+  if (localOffer == "offer3") {
+    document.querySelector(".chosen-offer").textContent = "8 numre af iFORM";
+  }
+
+  if (localOffer == "offer1") {
+    document.querySelector(".chosen-offer2").textContent = "4 numre af iFORM";
+  }
+  if (localOffer == "offer2") {
+    document.querySelector(".chosen-offer2").textContent = "6 numre af iFORM";
+  }
+  if (localOffer == "offer3") {
+    document.querySelector(".chosen-offer2").textContent = "8 numre af iFORM";
+  }
+
+  if (localOffer == "offer1") {
+    document.querySelector(".discount").textContent = "761 kr.";
+  }
+
+  if (localOffer == "offer1") {
+    document.querySelector(".local-price1").textContent = "149 kr.";
+  }
+
+  if (localOffer == "offer1") {
+    document.querySelector(".local-price2").textContent = "198,50 kr.";
+  }
+
+  if (localOffer == "offer1") {
+    document.querySelector(".local-price3").textContent = "198,50 kr.";
+  }
+
+  if (localOffer == "offer2") {
+    document.querySelector(".discount").textContent = "866 kr.";
+  }
+
+  if (localOffer == "offer2") {
+    document.querySelector(".local-price1").textContent = "199 kr.";
+  }
+
+  if (localOffer == "offer2") {
+    document.querySelector(".local-price2").textContent = "248,50 kr.";
+  }
+
+  if (localOffer == "offer2") {
+    document.querySelector(".local-price3").textContent = "248,50 kr.";
+  }
+
+  if (localOffer == "offer3") {
+    document.querySelector(".discount").textContent = "921 kr.";
+  }
+
+  if (localOffer == "offer3") {
+    document.querySelector(".local-price1").textContent = "299 kr.";
+  }
+
+  if (localOffer == "offer3") {
+    document.querySelector(".local-price2").textContent = "348,50 kr.";
+  }
+
+  if (localOffer == "offer3") {
+    document.querySelector(".local-price3").textContent = "348,50 kr.";
+  }
+
+  let localSize = localStorage.getItem("size");
+  if (localSize == "small") {
+    document.querySelector(".chosen-size").textContent = "Størrelse: Small";
+  }
+  if (localSize == "medium") {
+    document.querySelector(".chosen-size").textContent = "Størrelse: Medium";
+  }
+  if (localSize == "large") {
+    document.querySelector(".chosen-size").textContent = "Størrelse: Large";
+  }
+  if (localSize == "extra-large") {
+    document.querySelector(".chosen-size").textContent = "Størrelse: XL";
+  }
+
+  if (localSize == "small") {
+    document.querySelector(".chosen-size2").textContent = "Størrelse: Small";
+  }
+  if (localSize == "medium") {
+    document.querySelector(".chosen-size2").textContent = "Størrelse: Medium";
+  }
+  if (localSize == "large") {
+    document.querySelector(".chosen-size2").textContent = "Størrelse: Large";
+  }
+  if (localSize == "extra-large") {
+    document.querySelector(".chosen-size2").textContent = "Størrelse: XL";
+  }
+}
 
 const basket = document.querySelector("#basket");
 const info = document.querySelector("#info");
@@ -64,3 +160,5 @@ function breadcrumb() {
     document.querySelector(".breadcrumb_step3").classList.add("breadcrumb_current");
   }
 }
+
+function addOffer() {}
